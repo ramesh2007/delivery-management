3532119b65826301bb6669a5953afad41f44288e
Rakonexdev <dev2@rakonex.com>
2026-07-02 18:10:08 +0530

    User crud completed

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/change-password', [AuthController::class, 'changePassword']);
+    
+    // Simple User CRUD routes
+    Route::apiResource('users', UserController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
